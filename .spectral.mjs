1ac103582b23f26dfb63abbc871f59c7e8ac14e7
jesse@givetel.com <jesse@givetel.com>
2025-06-03 22:28:51 +1000

    Added .spectral.mjs
    Committed by jesses-code-adventures on behalf of jesse@givetel.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4Mjk4Ng/spectral.js?branch_slug=main&token=d259c5cb-a008-4ec1-90ae-400c5b243fce";
+export default { extends: ruleset };
